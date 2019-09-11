df41e0955ee8f4266278c15f861687d18ade2b3d
Roman Nikitin <nikrom2012@me.com>
2019-09-11 10:35:38 +0300

    Fixed resources path x2

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from distutils.core import setup
 setup(
     name='checklib',
     packages=['checklib'],
-    version='0.1.1',
+    package_data={
+        'checklib': ['resources/*']
+    },
+    version='0.1.2',
     license='MIT',
     description='Library for convenient checker writing',
     author='Roman Nikitin',
     author_email='nikrom.prog@gmail.com',
     url='https://github.com/pomo-mondreganto/',
-    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.1.1.tar.gz',
+    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.1.2.tar.gz',
     keywords=['AD', 'CTF', 'checker'],
     install_requires=[
         'requests',
