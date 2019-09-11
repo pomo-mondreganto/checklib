7f7f2123dc18ca35bfd6fc4c9046dda291a74ac2
pomo-mondreganto <1728390Roman>
2019-09-11 10:54:42 +0300

    Convenient imports from checklib

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     package_data={
         'checklib': ['resources/*']
     },
-    version='0.1.2',
+    version='0.1.3',
     license='MIT',
     description='Library for convenient checker writing',
     author='Roman Nikitin',
     author_email='nikrom.prog@gmail.com',
     url='https://github.com/pomo-mondreganto/',
-    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.1.2.tar.gz',
+    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.1.3.tar.gz',
     keywords=['AD', 'CTF', 'checker'],
     install_requires=[
         'requests',
