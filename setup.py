3bd7a11591688959d29990ece13e7977ce73ab58
pomo-mondreganto <1728390Roman>
2019-09-12 14:28:42 +0300

    imported new asserts

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     package_data={
         'checklib': ['resources/*']
     },
-    version='0.1.4',
+    version='0.1.5',
     license='MIT',
     description='Library for convenient checker writing',
     author='Roman Nikitin',
     author_email='nikrom.prog@gmail.com',
     url='https://github.com/pomo-mondreganto/',
-    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.1.4.tar.gz',
+    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.1.5.tar.gz',
     keywords=['AD', 'CTF', 'checker'],
     install_requires=[
         'requests',
