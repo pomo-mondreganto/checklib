a8a079c95cce3607c12cd838a3407f8ae83510df
Roman Nikitin <nikrom2012@me.com>
2019-09-12 14:25:15 +0300

    Added new asserts

--- checklib/assertions.py
+++ checklib/assertions.py
@@ -7,6 +7,11 @@ def assert_eq(a, b, public, status=checklib.status.Status.MUMBLE):
         utils.cquit(status, public, f'Equality assertion failed: {a} ({type(a)}) != {b} ({type(b)})')
 
 
+def assert_neq(a, b, public, status=checklib.status.Status.MUMBLE):
+    if a != b:
+        utils.cquit(status, public, f'Equality assertion failed: {a} ({type(a)}) != {b} ({type(b)})')
+
+
 def assert_gt(a, b, public, status=checklib.status.Status.MUMBLE):
     if not (a > b):
         utils.cquit(status, public, f'Inequality assertion failed: {a} <= {b}')
@@ -18,6 +23,11 @@ def assert_gte(a, b, public, status=checklib.status.Status.MUMBLE):
 
 
 def assert_in(what, where, public, status=checklib.status.Status.MUMBLE):
+    if what not in where:
+        utils.cquit(status, public, f'Contains assertion failed: {what} not in {where}')
+
+
+def assert_nin(what, where, public, status=checklib.status.Status.MUMBLE):
     if what not in where:
         utils.cquit(status, public, f'Contains assertion failed: {what} not in {where}')
 
