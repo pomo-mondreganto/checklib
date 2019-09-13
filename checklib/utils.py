3598be81aba300f4033f6845dec8c876779de8b8
pomo-mondreganto <1728390Roman>
2019-09-13 10:26:32 +0300

    Added context manager for exception handling

--- checklib/utils.py
+++ checklib/utils.py
@@ -1,6 +1,7 @@
 import sys
+from contextlib import contextmanager
 
-import checklib.status as status
+import checklib.status
 
 
 def cquit(code, public='', private=None):
@@ -9,5 +10,17 @@ def cquit(code, public='', private=None):
 
     print(public, file=sys.stdout)
     print(private, file=sys.stderr)
-    assert (type(code) == status.Status)
+    assert (type(code) == checklib.status.Status)
     sys.exit(code.value)
+
+
+@contextmanager
+def handle_exception(exc, public, private, status=checklib.status.Status.MUMBLE):
+    try:
+        yield True
+    except SystemError:
+        raise
+    except exc:
+        cquit(status, public, private)
+    except Exception:
+        raise
