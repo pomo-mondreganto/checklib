cb0e505515917f0c6c5e5f966128a60bb3bd7cd3
Roman Nikitin <nikrom2012@me.com>
2020-01-31 21:08:49 +0300

    Added README, fixed Checker class

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     package_data={
         'checklib': ['resources/*']
     },
-    version='0.2.0',
+    version='0.2.1',
     license='MIT',
     description='Library for convenient checker writing',
     author='Roman Nikitin',
     author_email='nikrom.prog@gmail.com',
     url='https://github.com/pomo-mondreganto/',
-    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.2.0.tar.gz',
+    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.2.1.tar.gz',
     keywords=['AD', 'CTF', 'checker'],
     install_requires=[
         'requests',
@@ -23,5 +23,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
