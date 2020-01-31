fb02535a497911b980b13edea377334fb73ec1fc
Roman Nikitin <nikrom2012@me.com>
2020-01-31 21:30:00 +0300

    renamed Checker to BaseChecker to avoid ambiguity

--- checklib/__init__.py
+++ checklib/__init__.py
@@ -7,7 +7,9 @@ from checklib.assertions import (
     assert_nin,
     assert_in_list_dicts,
 )
-
+from checklib.checker import (
+    BaseChecker,
+)
 from checklib.generators import (
     rnd_bytes,
     rnd_string,
@@ -16,22 +18,15 @@ from checklib.generators import (
     rnd_useragent,
     get_initialized_session,
 )
-
 from checklib.http import (
     get_json,
     get_text,
     check_response,
 )
-
 from checklib.status import (
     Status,
 )
-
 from checklib.utils import (
     cquit,
     handle_exception,
-)
-
-from checklib.checker import (
-    Checker,
 )
--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     package_data={
         'checklib': ['resources/*']
     },
-    version='0.2.1',
+    version='0.2.2',
     license='MIT',
     description='Library for convenient checker writing',
     author='Roman Nikitin',
     author_email='nikrom.prog@gmail.com',
     url='https://github.com/pomo-mondreganto/',
-    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.2.1.tar.gz',
+    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.2.2.tar.gz',
     keywords=['AD', 'CTF', 'checker'],
     install_requires=[
         'requests',
