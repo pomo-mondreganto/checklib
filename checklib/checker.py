001005c7e6cb95dbd1941fe5a513f043f30919c3
Roman Nikitin <nikrom2012@me.com>
2020-10-12 14:42:13 +0300

    Fixed potential json errors, added info method to BaseChecker added session getter to checker with cleanup in cquit

--- checklib/checker.py
+++ checklib/checker.py
@@ -1,4 +1,5 @@
 import checklib.assertions
+import checklib.generators
 import checklib.http
 import checklib.status
 
@@ -15,12 +16,15 @@ class BaseChecker(checklib.assertions.CheckerAssertionsMixin, checklib.http.Chec
         self.status = checklib.status.Status.OK.value
         self.public = ''
         self.private = ''
+        self._sessions = []
 
     @staticmethod
     def get_check_finished_exception():
         return CheckFinished
 
     def action(self, action, *args, **kwargs):
+        if action == 'info':
+            return self.info(*args, **kwargs)
         if action == 'check':
             return self.check(*args, **kwargs)
         elif action == 'put':
@@ -29,6 +33,9 @@ class BaseChecker(checklib.assertions.CheckerAssertionsMixin, checklib.http.Chec
             return self.get(*args, **kwargs)
         else:
             self.cquit(checklib.status.Status.ERROR, 'Checker failed', f'Invalid action: {action}')
+
+    def info(self, *_args, **_kwargs):
+        raise NotImplementedError('You must implement this method')
 
     def check(self, *_args, **_kwargs):
         raise NotImplementedError('You must implement this method')
@@ -39,6 +46,11 @@ class BaseChecker(checklib.assertions.CheckerAssertionsMixin, checklib.http.Chec
     def get(self, *_args, **_kwargs):
         raise NotImplementedError('You must implement this method')
 
+    def get_initialized_session(self):
+        sess = checklib.generators.get_initialized_session()
+        self._sessions.append(sess)
+        return sess
+
     def cquit(self, status, public='', private=None):
         if private is None:
             private = public
@@ -46,5 +58,9 @@ class BaseChecker(checklib.assertions.CheckerAssertionsMixin, checklib.http.Chec
         self.status = status.value
         self.public = public
         self.private = private
+
+        for sess in self._sessions:
+            sess.close()
+        self._sessions = []
 
         raise self.get_check_finished_exception()
