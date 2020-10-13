c198b16a2ad42c403f80863fa8e52d48ff9bb425
Roman Nikitin <nikrom2012@me.com>
2020-10-13 09:32:49 +0300

    Introduced typing, added default INFO action implementation

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     package_data={
         'checklib': ['resources/*']
     },
-    version='0.4.0',
+    version='0.5.0',
     license='MIT',
     description='Library for convenient checker writing',
     author='Roman Nikitin',
     author_email='nikrom.prog@gmail.com',
     url='https://github.com/pomo-mondreganto/',
-    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.4.0.tar.gz',
+    download_url='https://github.com/pomo-mondreganto/checklib/archive/v_0.5.0.tar.gz',
     keywords=['AD', 'CTF', 'checker'],
     install_requires=[
         'requests',
